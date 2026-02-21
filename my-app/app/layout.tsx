d66c763840ace5328a1a6bcf7d8ac1c0e72a51c4
blurissa <blurissa@gmail.com>
2026-02-21 15:38:23 -0500

    Changing background color

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        style={{ backgroundColor: "#D2DCB6" }} // background color for the entire website
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navigation />
